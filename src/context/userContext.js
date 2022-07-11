8c23aba2b968c631b17c8d502546aa25570d7581
PujaGKundu <pujakundu100@gmail.com>
2022-07-11 20:24:39 +0530

    Gallery website updated content

--- src/context/userContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-let UserContext = createContext();
-
-let UserProvider = UserContext.Provider;
-let UserConsumer = UserContext.Consumer;
-
-export { UserProvider, UserConsumer };
-export default UserContext;
